e0d76a02b111b746e90f5e17652287e308458c4c
daffy <31366597+prakashvr1998@users.noreply.github.com>
2018-08-28 17:05:12 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,13 @@
+print('Welcome Guys')
+print('Please Enter your name:')
+name =input()
+print ('Please Enter your number:')
+num= input()
+print('Please Enter your age:')
+age=input()
+print('Hai ' + name + ' Your Basic Details are Given Below  \nYour Number is ' + num + '\n Your Age is ' + age)
+if age<20:
+    print('you are teenager')
+else:
+    print('you are citizen')
+
